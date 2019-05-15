426af789e53c95bcd0b2d887b4baf6849b7449e5
AllanMakau <alan.makau@gmail.com>
2019-05-14 23:33:44 -0300

    consumindo api cargo

--- src/app/app.routers.ts
+++ src/app/app.routers.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { UsuarioConsultaComponent } from './usuario/usuario-consulta/usuario-consulta.component';
 import { ResumoComponent } from './resumo/resumo.component';
 import { UsuarioCadastroComponent } from './usuario/usuario-cadastro/usuario-cadastro.component';
+import { CargoListaComponent } from './cargo/cargo-lista/cargo-lista.component';
 
 
 export const ROUTES: Routes = [
@@ -12,5 +13,6 @@ export const ROUTES: Routes = [
     {path: 'about' , component: AboutComponent},
     {path: 'usuarioConsulta' , component: UsuarioConsultaComponent},
     {path: 'resumo' , component: ResumoComponent},
-    {path: 'usuarioCadastro' , component: UsuarioCadastroComponent}
+    {path: 'usuarioCadastro' , component: UsuarioCadastroComponent},
+    {path: 'cargoLista' , component: CargoListaComponent}
 ]
---
+++ src/app/cargo/cargo-lista/cargo-lista.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crm-cargo-lista',
+  templateUrl: './cargo-lista.component.html'
+})
+export class CargoListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
