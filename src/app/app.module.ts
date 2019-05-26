4b7a509d4408b6926f0b8cf6b7902c13d2ee90f9
AllanMakau <alan.makau@gmail.com>
2019-05-26 00:31:22 -0300

    cadastrando usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule}  from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -39,7 +40,7 @@ import { usuarioService } from './usuario/usuarioService';
   imports: [
     BrowserModule,
     RouterModule.forRoot(ROUTES),
-    HttpClientModule
+    HttpClientModule,FormsModule
   ],
   providers: [cargoService, usuarioService],
   bootstrap: [AppComponent]
--- src/app/usuario/usuarioService.ts
+++ src/app/usuario/usuarioService.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API } from '../app.api';
 import { Observable } from 'rxjs';
+import { user } from './usuariomodel';
 
 
 
@@ -18,14 +19,16 @@ export class usuarioService{
 
     
     listar(){
-
         return this.http.get<any[]>( `${API}`+"/usuarios")
     }
 
 
-    salvar(usuario:any){
+    cadastrar(usuario:user){
         return this.http.post(this.urlUsuario,usuario);
       }
 
+    editar(usuario:user){
+        return this.http.put(this.urlUsuario,usuario);
+    }
 
 }
